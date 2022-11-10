d5c8807dd03681b499d27774ac7137e032e95126
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2022-11-10 21:30:21 +0800

    fix: Change keybind for Lua code prompt

--- awesome/.config/awesome/configuration/keys/global.lua
+++ awesome/.config/awesome/configuration/keys/global.lua
@@ -210,18 +210,18 @@ globalKeys = gears.table.join(
 
 	-- Prompt
 	awful.key({ meta },            "r",     function () awful.screen.focused().mypromptbox:run() end,
-		{description = "run prompt", group = "launcher"})
+		{description = "run prompt", group = "launcher"}),
 
-	--awful.key({ meta }, "x",
-		--function ()
-			--awful.prompt.run {
-				--prompt       = "Run Lua code: ",
-				--textbox      = awful.screen.focused().mypromptbox.widget,
-				--exe_callback = awful.util.eval,
-				--history_path = awful.util.get_cache_dir() .. "/history_eval"
-			--}
-		--end,
-		--{description = "lua execute prompt", group = "awesome"})
+	awful.key({ meta , alt }, "x",
+		function ()
+			awful.prompt.run {
+				prompt       = "Run Lua code: ",
+				textbox      = awful.screen.focused().mypromptbox.widget,
+				exe_callback = awful.util.eval,
+				history_path = awful.util.get_cache_dir() .. "/history_eval"
+			}
+		end,
+		{description = "lua execute prompt", group = "awesome"})
 	-- Menubar
 	--awful.key({ meta }, "d", function() menubar.show() end,
 		--{description = "show the menubar", group = "launcher"})
