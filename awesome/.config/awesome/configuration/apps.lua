152f476d62f0965471844e9e6544f7e37176050a
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-15 03:04:48 +0800

    Format awesome/apps.lua config

--- awesome/.config/awesome/configuration/apps.lua
+++ awesome/.config/awesome/configuration/apps.lua
@@ -1,39 +1,40 @@
-local dpi = require("beautiful.xresources").apply_dpi
-local menubar = require("menubar")
+local dpi              = require("beautiful.xresources").apply_dpi
+local menubar          = require("menubar")
 
-apps = {
+apps                   = {
 
 	-- Your default terminal
-	terminal                      = "kitty" or "alacritty" or os.getenv("TERM"),
+	terminal      = "kitty" or "alacritty" or os.getenv("TERM"),
 
 	-- Your default text editor
-	editor                        = os.getenv("EDITOR") or "nvim" or "vim",
+	editor        = os.getenv("EDITOR") or "nvim" or "vim",
 
 	-- Scratch window
-	scratch = os.getenv("EDITOR") or "nvim" or "vim" .. " -c setlocal buftype=nofile noswapfile bufhidden=wipe nobuflisted",
+	scratch       = os.getenv("EDITOR") or "nvim" or
+		"vim" .. " -c setlocal buftype=nofile noswapfile bufhidden=wipe nobuflisted",
 
 	-- Your default file explorer
-	explorer                      = "thunar" or "pcmanfm",
+	explorer      = "thunar" or "pcmanfm",
 
-	launcher                      = "rofi -show-icons -show combi -combi-modi 'window,drun,run'",
+	launcher      = "rofi -show-icons -show combi -combi-modi 'window,drun,run,emoji'",
 
-	calculator                    = "rofi -show calc",
+	calculator    = "rofi -show calc",
 
-	scrlocker                     = os.getenv("HOME") .. "/.local/bin/scripts/lock",
+	scrlocker     = os.getenv("HOME") .. "/.local/scripts/lock",
 
-	powermenu					  = os.getenv("HOME") .. "/.local/bin/powermenu.sh",
+	powermenu     = os.getenv("HOME") .. "/.local/powermenu.sh",
 
-	browser                       = "firefox",
+	browser       = "firefox",
 
-	chat_app					  = "signal-desktop",
+	chat_app      = "signal-desktop",
 
 	term_explorer = "ranger"
 }
 
-apps.editor_cmd   = apps.terminal .. " -e " .. apps.editor
-apps.explorer_cmd = apps.terminal .. " -e " .. apps.explorer
+apps.editor_cmd        = apps.terminal .. " -e " .. apps.editor
+apps.explorer_cmd      = apps.terminal .. " -e " .. apps.explorer
 apps.term_explorer_cmd = apps.terminal .. " -e " .. apps.term_explorer
-apps.scratch = apps.terminal .. " -e " .. apps.scratch
+apps.scratch           = apps.terminal .. " -e " .. apps.scratch
 menubar.utils.terminal = apps.terminal -- Set the terminal for applications that require it
 
 
